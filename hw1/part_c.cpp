77ce6ec4b272caf0860addd65295f331c2c12849
jasonhojh1122 <jasonhojh1122@gmail.com>
2020-10-05 21:08:37 +0800

    update rotation

--- hw1/part_c.cpp
+++ hw1/part_c.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <unistd.h>
 #include "/usr/local/Aria/include/Aria.h"
 
-const double D_T = 1;
-const double MAX_VEL = 400;
-const double MAX_ROT_VEL = 200;
-const double ACCEL = 20;
+const int D_T = 50; // ms
+const double MAX_VEL = 350;
+const double MAX_ROT_VEL = 10;
+const double ACCEL = 100;
 
 class RobotInfo {
 public:
@@ -19,23 +20,25 @@ public:
 class KeyPressCallBack {
 public:
 	KeyPressCallBack(ArRobot& robot, RobotInfo& robotInfo) : robot(robot), robotInfo(robotInfo) {
-
-	} 
+		updateRobotVel();
+	}
     void key_w() {
-		robotInfo.vel = checkVelBound(robotInfo.vel + ACCEL * D_T);
-		updateRobotMovement();
+		robotInfo.vel = checkVelBound(robotInfo.vel + ACCEL * D_T / 1000);
+		robotInfo.rotVel = 0;
+		updateRobotVel();
 	}
 	void key_s() {
-		robotInfo.vel = checkVelBound(robotInfo.vel - ACCEL * D_T);
-		updateRobotMovement();
+		robotInfo.vel = checkVelBound(robotInfo.vel - ACCEL * D_T / 1000);
+		robotInfo.rotVel = 0;
+		updateRobotVel();
 	}
 	void key_a() {
-		robotInfo.rotVel = checkRotVelBound(robotInfo.rotVel + ACCEL * D_T);
-		updateRobotMovement();
+		robotInfo.rotVel = MAX_ROT_VEL;
+		updateRobotVel();
 	}
 	void key_d() {
-		robotInfo.rotVel = checkRotVelBound(robotInfo.rotVel - ACCEL * D_T);
-		updateRobotMovement();
+		robotInfo.rotVel = -MAX_ROT_VEL;
+		updateRobotVel();
 	}
 
 	ArRobot& robot;
@@ -58,7 +61,7 @@ private:
 		else
 			return rotVel;
 	}
-	void updateRobotMovement() {
+	void updateRobotVel() {
 		robot.lock();
 		robot.setVel(robotInfo.vel);
 		robot.setRotVel(robotInfo.rotVel);
@@ -88,20 +91,18 @@ int main(int argc, char **argv) {
 
 	robot.setAbsoluteMaxTransVel(MAX_VEL);
 	robot.setAbsoluteMaxRotVel(MAX_VEL);
-
-	robot.runAsync(true);
-	
-	KeyPressCallBack keyPressCallBack = {robot, robotInfo};
-    ArFunctorC<KeyPressCallBack> key_w_pressed(keyPressCallBack, &KeyPressCallBack::key_w);
-	ArFunctorC<KeyPressCallBack> key_s_pressed(keyPressCallBack, &KeyPressCallBack::key_s);
-	ArFunctorC<KeyPressCallBack> key_a_pressed(keyPressCallBack, &KeyPressCallBack::key_a);
-	ArFunctorC<KeyPressCallBack> key_d_pressed(keyPressCallBack, &KeyPressCallBack::key_d);
 	
 	ArKeyHandler *keyHandler = Aria::getKeyHandler();
     if (keyHandler == NULL) {
         keyHandler = new ArKeyHandler;
         Aria::setKeyHandler(keyHandler);
     }
+
+	KeyPressCallBack keyPressCallBack = {robot, robotInfo};
+    ArFunctorC<KeyPressCallBack> key_w_pressed(keyPressCallBack, &KeyPressCallBack::key_w);
+	ArFunctorC<KeyPressCallBack> key_s_pressed(keyPressCallBack, &KeyPressCallBack::key_s);
+	ArFunctorC<KeyPressCallBack> key_a_pressed(keyPressCallBack, &KeyPressCallBack::key_a);
+	ArFunctorC<KeyPressCallBack> key_d_pressed(keyPressCallBack, &KeyPressCallBack::key_d);
 
     keyHandler->addKeyHandler('w', &key_w_pressed);
 	keyHandler->addKeyHandler('s', &key_s_pressed);
@@ -111,8 +112,16 @@ int main(int argc, char **argv) {
 	robot.attachKeyHandler(keyHandler);
 	printf("You may press escape to exit\n");
 
+	robot.runAsync(false);
 
-	robot.waitForRunExit();
+	while (true) {
+		robot.lock();
+		robotInfo.rotVel = 0;
+		robot.setRotVel(0);
+		robot.unlock();
+		usleep(D_T);
+	}
+
 	Aria::shutdown();
 	Aria::exit(0);
 }
