382ffc965ae571ca5c7111318a86ed14bb2c793a
jasonhojh1122 <jasonhojh1122@gmail.com>
2020-10-08 22:08:32 +0800

    add part_d.cpp

---
+++ hw1/part_d.cpp
@@ -0,0 +1,173 @@
+#include <iostream>
+#include <unistd.h>
+#include "/usr/local/Aria/include/Aria.h"
+
+const int D_T = 50; // ms
+const double MAX_VEL = 350;
+const double MAX_ROT_VEL = 10;
+const double ACCEL = 100;
+const double MIN_DIST = 50;
+
+class RobotController {
+public:
+
+	double vel;
+	double rotVel;
+
+	ArRobot& robot;
+	ArRangeDevice* mySonar;
+	double distThreshold;
+	double range;
+	double angle;
+
+	RobotController (ArRobot& robot, double distThreshold)
+	: robot(robot), distThreshold(distThreshold) {
+		vel = 0;
+		rotVel = 0;
+		mySonar = robot.findRangeDevice("sonar");
+		if (mySonar == NULL) {
+			std::cout << "No sonar device found.";
+			exit(1);
+		}
+	}
+
+	void readSurrondingDist() {
+		range = mySonar->currentReadingPolar(0, 359, &angle);
+		std::cout << range << ' ' << angle << '\n';
+	}
+
+	bool obstacleInSameDirection() {
+		if (angle < 60 && angle > -60 && vel > 0) {
+			return true;
+		}
+		else if ((angle > 150 || angle < -150) && vel < 0){
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	bool shouldStop() {
+		readSurrondingDist();
+		if (range < distThreshold && obstacleInSameDirection())
+			return true;
+		else
+			return false;
+	}
+};
+
+class KeyPressCallBack {
+public:
+	KeyPressCallBack(ArRobot& robot, RobotController& controller)
+	: robot(robot), controller(controller) {
+		updateRobotVel();
+	}
+    void key_w() {
+		controller.vel = checkVelBound(controller.vel + ACCEL * D_T / 1000);
+		controller.rotVel = 0;
+		updateRobotVel();
+	}
+	void key_s() {
+		controller.vel = checkVelBound(controller.vel - ACCEL * D_T / 1000);
+		controller.rotVel = 0;
+		updateRobotVel();
+	}
+	void key_a() {
+		controller.rotVel = MAX_ROT_VEL;
+		updateRobotVel();
+	}
+	void key_d() {
+		controller.rotVel = -MAX_ROT_VEL;
+		updateRobotVel();
+	}
+
+	ArRobot& robot;
+	RobotController& controller;
+
+	double checkVelBound(double vel) {
+		if (vel > MAX_VEL)
+			return MAX_VEL;
+		else if (vel < -MAX_VEL)
+			return -MAX_VEL;
+		else
+			return vel;
+	}
+	double checkRotVelBound(double rotVel) {
+		if (rotVel > MAX_ROT_VEL)
+			return MAX_ROT_VEL;
+		else if (rotVel < -MAX_ROT_VEL)
+			return -MAX_ROT_VEL;
+		else
+			return rotVel;
+	}
+	void updateRobotVel() {
+		robot.lock();
+		robot.setVel(controller.vel);
+		robot.setRotVel(controller.rotVel);
+		robot.unlock();
+	}
+};
+
+int main(int argc, char **argv) {
+	ArRobot robot;
+	ArSonarDevice sonar;
+	
+	robot.addRangeDevice(&sonar);
+
+	Aria::init();
+	
+	ArSimpleConnector connector(&argc,argv);
+
+	if (!connector.connectRobot(&robot)){
+		printf("Could not connect to robot... exiting\n");
+		Aria::shutdown();
+		Aria::exit(1);
+	}
+
+	robot.comInt(ArCommands::ENABLE, 1);
+	robot.comInt(ArCommands::SONAR, 1);
+
+	robot.setAbsoluteMaxTransVel(MAX_VEL);
+	robot.setAbsoluteMaxRotVel(MAX_VEL);
+	
+	ArKeyHandler *keyHandler = Aria::getKeyHandler();
+    if (keyHandler == NULL) {
+        keyHandler = new ArKeyHandler;
+        Aria::setKeyHandler(keyHandler);
+    }
+
+	RobotController controller = {robot, 1000};
+
+	KeyPressCallBack keyPressCallBack = {robot, controller};
+    ArFunctorC<KeyPressCallBack> key_w_pressed(keyPressCallBack, &KeyPressCallBack::key_w);
+	ArFunctorC<KeyPressCallBack> key_s_pressed(keyPressCallBack, &KeyPressCallBack::key_s);
+	ArFunctorC<KeyPressCallBack> key_a_pressed(keyPressCallBack, &KeyPressCallBack::key_a);
+	ArFunctorC<KeyPressCallBack> key_d_pressed(keyPressCallBack, &KeyPressCallBack::key_d);
+
+    keyHandler->addKeyHandler('w', &key_w_pressed);
+	keyHandler->addKeyHandler('s', &key_s_pressed);
+	keyHandler->addKeyHandler('a', &key_a_pressed);
+	keyHandler->addKeyHandler('d', &key_d_pressed);
+
+	robot.attachKeyHandler(keyHandler);
+	printf("You may press escape to exit\n");
+
+	robot.runAsync(false);
+
+	while (true) {
+		robot.lock();
+		controller.rotVel = 0;
+		robot.setRotVel(0);
+		robot.unlock();
+		if (controller.shouldStop()) {
+			std::cout << "should stop\n";
+			controller.vel *= 0.9;
+			keyPressCallBack.updateRobotVel();
+		}
+		usleep(D_T);
+	}
+
+	Aria::shutdown();
+	Aria::exit(0);
+}
